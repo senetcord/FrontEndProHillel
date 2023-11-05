a457c837d527e3ac93be175be62969ca1d5798fd
senetcord <senetcord@gmail.com>
2023-11-05 13:52:22 +0200

    finished Slider

--- dz#13/functional/script.js
+++ dz#13/functional/script.js
@@ -1,69 +1,74 @@
 const prevButton = document.querySelector(".js--prev");
 const nextButton = document.querySelector(".js--next");
-const imageElement = document.querySelector(".js--img");
+const image = document.querySelector(".js--img");
 const dotsContainer = document.querySelector(".js--dots");
 
-const images = {
-  1: "/img/undergroundBuilding.jpg",
-  2: "/img/winterApocalypse.jpg",
-  3: "/img/roomInBunker.jpg",
-  4: "/img/surveillanceTower.jpg",
-};
-const totalImages = Object.keys(images).length;
+const images = [
+  "./img/undergroundBuilding.jpg",
+  "./img/roomInBunker.jpg",
+  "./img/winterApocalypse.jpg",
+  "./img/surveillanceTower.jpg",
+];
 
-const createDot = (id) => {
-  const dot = document.createElement("li");
-  dot.id = id;
-  return dot;
+let currentImageIndex = 0;
+
+const createDot = (index) => {
+  const newDot = document.createElement("li");
+  newDot.id = "pic_" + index;
+  newDot.addEventListener("click", handleDotClick);
+  return newDot;
 };
 
-const createDots = () => {
-  for (let i = 1; i <= totalImages; i++) {
-    const dot = createDot(`pic_${i}`);
-    dotsContainer.appendChild(dot);
+const handleDotClick = (e) => {
+  currentImageIndex = Number(e.target.id.match(/\d+/));
+  updateSlide();
+  highlightHandler();
+  checkSlider();
+};
+
+const updateSlide = () => {
+  image.src = images[currentImageIndex];
+};
+
+const checkSlider = () => {
+  prevButton.style.visibility = currentImageIndex === 0 ? "hidden" : "visible";
+  nextButton.style.visibility =
+    currentImageIndex === images.length - 1 ? "hidden" : "visible";
+};
+
+const highlightHandler = () => {
+  for (let i = 0; i < images.length; i++) {
+    dotsContainer.children[i].classList.toggle(
+      "active",
+      i === currentImageIndex
+    );
   }
 };
 
-const checkButtonVisibility = () => {
-  prevButton.style.visibility = currentSlide === 1 ? "hidden" : "visible";
-  nextButton.style.visibility =
-    currentSlide === totalImages ? "hidden" : "visible";
+const handleNextBtn = () => {
+  currentImageIndex++;
+  updateSlide();
+  checkSlider();
+  highlightHandler();
 };
 
-const highlightDot = () => {
-  const currentDot = document.querySelector(`#pic_${currentSlide}`);
-  currentDot.classList.add("active");
+const handlePrevBtn = () => {
+  currentImageIndex--;
+  updateSlide();
+  checkSlider();
+  highlightHandler();
 };
 
-const removeDotHighlight = (direction, slide) => {
-  const previousSlide = direction ? slide - 1 : slide + 1;
-  const previousDot = document.querySelector(`#pic_${previousSlide}`);
-  previousDot.classList.remove("active");
+prevButton.addEventListener("click", handlePrevBtn);
+nextButton.addEventListener("click", handleNextBtn);
+
+const init = () => {
+  for (let i = 0; i < images.length; i++) {
+    const newDot = createDot(i);
+    dotsContainer.appendChild(newDot);
+  }
+  checkSlider();
+  highlightHandler();
 };
 
-const handleNextButtonClick = () => {
-  imageElement.src = "." + images[++currentSlide];
-  checkButtonVisibility();
-  highlightDot();
-  removeDotHighlight(true, currentSlide);
-};
-
-const handlePrevButtonClick = () => {
-  imageElement.src = "." + images[--currentSlide];
-  checkButtonVisibility();
-  highlightDot();
-  removeDotHighlight(false, currentSlide);
-};
-
-nextButton.addEventListener("click", handleNextButtonClick);
-prevButton.addEventListener("click", handlePrevButtonClick);
-
-const initializeSlider = () => {
-  createDots();
-  checkButtonVisibility();
-  highlightDot();
-};
-
-let currentSlide = 1;
-
-initializeSlider();
+init();
--- dz#13/iterative/script.js
+++ dz#13/iterative/script.js
@@ -1,57 +1,67 @@
 const prevButton = document.querySelector(".js--prev");
 const nextButton = document.querySelector(".js--next");
-const imageElement = document.querySelector(".js--img");
+const image = document.querySelector(".js--img");
 const dotsContainer = document.querySelector(".js--dots");
 
-const images = {
-  1: "/img/undergroundBuilding.jpg",
-  2: "/img/winterApocalypse.jpg",
-  3: "/img/roomInBunker.jpg",
-  4: "/img/surveillanceTower.jpg",
-};
-const totalImages = Object.keys(images).length;
+const images = [
+  "./img/undergroundBuilding.jpg",
+  "./img/roomInBunker.jpg",
+  "./img/winterApocalypse.jpg",
+  "./img/surveillanceTower.jpg",
+];
 
-for (let i = 1; i <= totalImages; i++) {
-  const dot = document.createElement("li");
-  dot.id = `pic_${i}`;
-  dotsContainer.appendChild(dot);
+let currentImageIndex = 0;
+
+for (let i = 0; i < images.length; i++) {
+  const newDot = document.createElement("li");
+  newDot.id = "pic_" + i;
+  newDot.addEventListener("click", handleDotClick);
+  dotsContainer.appendChild(newDot);
 }
 
-let currentSlide = 1;
+function handleDotClick(e) {
+  currentImageIndex = Number(e.target.id.match(/\d+/));
+  updateSlide();
+  highlightHandler();
+  checkSlider();
+}
 
-function checkButtonVisibility() {
-  prevButton.style.visibility = currentSlide === 1 ? "hidden" : "visible";
+function updateSlide() {
+  image.src = images[currentImageIndex];
+}
+
+function checkSlider() {
+  prevButton.style.visibility = currentImageIndex === 0 ? "hidden" : "visible";
   nextButton.style.visibility =
-    currentSlide === totalImages ? "hidden" : "visible";
+    currentImageIndex === images.length - 1 ? "hidden" : "visible";
 }
 
-function highlightDot() {
-  const currentDot = document.querySelector(`#pic_${currentSlide}`);
-  currentDot.classList.add("active");
+checkSlider();
+
+function highlightHandler() {
+  for (let i = 0; i < images.length; i++) {
+    dotsContainer.children[i].classList.toggle(
+      "active",
+      i === currentImageIndex
+    );
+  }
 }
 
-function removeDotHighlight(direction, slide) {
-  const previousSlide = direction ? slide - 1 : slide + 1;
-  const previousDot = document.querySelector(`#pic_${previousSlide}`);
-  previousDot.classList.remove("active");
+highlightHandler();
+
+function handleNextBtn() {
+  currentImageIndex++;
+  updateSlide();
+  checkSlider();
+  highlightHandler();
 }
 
-function handleNextButtonClick() {
-  imageElement.src = "." + images[++currentSlide];
-  checkButtonVisibility();
-  highlightDot();
-  removeDotHighlight(true, currentSlide);
+function handlePrevBtn() {
+  currentImageIndex--;
+  updateSlide();
+  checkSlider();
+  highlightHandler();
 }
 
-function handlePrevButtonClick() {
-  imageElement.src = "." + images[--currentSlide];
-  checkButtonVisibility();
-  highlightDot();
-  removeDotHighlight(false, currentSlide);
-}
-
-nextButton.addEventListener("click", handleNextButtonClick);
-prevButton.addEventListener("click", handlePrevButtonClick);
-
-checkButtonVisibility();
-highlightDot();
+prevButton.addEventListener("click", handlePrevBtn);
+nextButton.addEventListener("click", handleNextBtn);
