16203a5807d0532057ceeb075f1015dcade2540d
senetcord <senetcord@gmail.com>
2023-12-22 17:51:35 +0200

    Added MongoDB, v02

--- dz23/BackEnd/index.js
+++ dz23/BackEnd/index.js
@@ -1,15 +1,79 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const TodoList = require("./json/data.json");
+app.use(express.static("public"));
 
+let TodoList = require("./json/data.json");
+
+const LOGIN = "MongoLearn";
+const PASSWORD = "s6yKWs8bmflz3y1N";
+const URL = `mongodb+srv://${LOGIN}:${PASSWORD}@cluster0.tcgkuyk.mongodb.net/?retryWrites=true&w=majority`;
 const PORT = 5000;
+
+const Schema = mongoose.Schema;
+
+const TodoListSchema = Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+
+const TodoModel = mongoose.model("TodoList", TodoListSchema);
+
+mongoose.connect(URL).then(() => console.log("connected tp DB"));
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}/`);
 });
 
 app.get("/todoList", (req, res) => {
   res.send(TodoList);
+});
+
+app.post("/todoList", (req, res) => {
+  TodoList.push({
+    _id: Math.random(),
+    text: req.body.text,
+    checked: false,
+  });
+
+  res.send({ message: "OK!" });
+});
+
+app.delete("/todoList/:id", (req, res) => {
+  const id = +req.params.id;
+  TodoList = TodoList.filter((item) => item._id !== id);
+  res.send({ message: "OK!" });
+});
+
+app.delete("/todoList", (req, res) => {
+  TodoList = [];
+  res.send({ message: "OK!" });
+});
+
+app.put("/todoList/:id", (req, res) => {
+  const id = +req.params.id;
+  TodoList = TodoList.map((item) => {
+    if (item._id === id) {
+      return {
+        ...item,
+        checked: !item.checked,
+      };
+    } else {
+      return item;
+    }
+  });
+  res.send({ message: "OK!" });
 });
--- dz23/FrontEnd/src/js/main.js
+++ dz23/FrontEnd/src/js/main.js
@@ -1,15 +1,26 @@
 import axios from "axios";
 import "../scss/styles.scss";
 
+const inputForm = document.querySelector(".js--inputForm");
 const inputArea = document.getElementById("inputArea");
 const addTodoBtn = document.getElementById("addTodoBtn");
 const clearAllTodoBtn = document.getElementById("clearAllTodoBtn");
 const todoList = document.querySelector(".js--todoList");
 
-document.addEventListener("DOMContentLoaded", async function () {
-  const res = await axios.get("http://localhost:5000/todoList");
-  renderTodoList(res.data);
+const URL = "http://localhost:5000/todoList";
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  handleAddTodo();
 });
+
+document.addEventListener("DOMContentLoaded", refreshList);
+
+async function refreshList() {
+  todoList.innerHTML = "";
+  const res = await axios.get(URL);
+  renderTodoList(res.data);
+}
 
 function renderTodoList(data) {
   for (let i = 0; i < data.length; i++) {
@@ -34,6 +45,11 @@ function renderTodoItem(text, checkedStatus, id) {
   input.type = "checkbox";
   input.checked = checkedStatus;
 
+  input.addEventListener("click", async () => {
+    const req = await axios.put(`${URL}/${li.id}`);
+    refreshList();
+  });
+
   const span = document.createElement("span");
   span.classList.add("ms-1", "js--todoText");
   span.textContent = text;
@@ -42,9 +58,29 @@ function renderTodoItem(text, checkedStatus, id) {
   button.classList.add("btn-close", "ms-auto", "js--todoDeleteBtn");
   button.setAttribute("aria-label", "Close");
 
+  button.addEventListener("click", async () => {
+    const req = await axios.delete(`${URL}/${li.id}`);
+    refreshList();
+  });
+
   li.appendChild(input);
   li.appendChild(span);
   li.appendChild(button);
 
   return li;
 }
+
+addTodoBtn.addEventListener("click", handleAddTodo);
+
+async function handleAddTodo() {
+  const inputText = inputArea.value;
+  const req = await axios.post(URL, {
+    text: inputText,
+  });
+  refreshList();
+}
+
+clearAllTodoBtn.addEventListener("click", async () => {
+  const req = await axios.delete(`${URL}`);
+  refreshList();
+});
